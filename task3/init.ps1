0f8a44f096537fa0b7a640c6a23bd622ec53d5a3
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-18 19:52:24 +0300

    placing west europe parameter and adding test creating storage account

--- task3/init.ps1
+++ task3/init.ps1
@@ -1,6 +1,6 @@
 ﻿Login-AzureRmAccount
 
-New-AzureRmResourceGroup -Name examplegroup -Location "South Central US"
+New-AzureRmResourceGroup -Name examplegroup -Location "West Europe"
 New-AzureRmResourceGroupDeployment -ResourceGroupName examplegroup -TemplateFile init.json
 
 #https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-create-first-template
