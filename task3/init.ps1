7a90b61ff2fc9d10efc001ebbe5ffd80293a7616
Usagich <liloddy@gmail.com>
2018-01-19 10:48:34 +0300

    Add files via upload

--- task3/init.ps1
+++ task3/init.ps1
@@ -2,5 +2,5 @@
 Set-AzureRmContext -SubscriptionId "b1d40bc1-2977-4394-b374-fe62498046e2" 
 
 
-New-AzureRmResourceGroup -Name examplegroup -Location "West Europe"
-New-AzureRmResourceGroupDeployment -ResourceGroupName examplegroup -TemplateFile init.json
+New-AzureRmResourceGroup -Name task3RG -Location "West Europe"
+New-AzureRmResourceGroupDeployment -ResourceGroupName task3RG -TemplateFile "C:\Users\Usagi\Desktop\azure-training-master\azure-training-master\task3\init.json"
