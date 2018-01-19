0e6eea81163fd4c6e295e9e45787c9a80231830f
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-19 13:16:37 +0300

    debugging task3

--- task3/init.ps1
+++ task3/init.ps1
@@ -3,4 +3,4 @@ Set-AzureRmContext -SubscriptionId "b1d40bc1-2977-4394-b374-fe62498046e2"
 
 
 New-AzureRmResourceGroup -Name task3RG -Location "West Europe"
-New-AzureRmResourceGroupDeployment -ResourceGroupName task3RG -TemplateFile "D:\azure_training\azure-training\task3\init.json" -TemplateParameterFile "D:\azure_training\azure-training\task3\init.parameters.json" 
+New-AzureRmResourceGroupDeployment -ResourceGroupName task3RG -TemplateFile "https://raw.githubusercontent.com/Usagich/azure-training/master/task3/init.json"
