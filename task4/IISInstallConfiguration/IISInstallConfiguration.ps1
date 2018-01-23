c34290ad798db8903f032609191e3f2e685e4d5f
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-23 19:54:15 +0300

    debugging DSC extention

--- task4/IISInstallConfiguration/IISInstallConfiguration.ps1
+++ task4/IISInstallConfiguration/IISInstallConfiguration.ps1
@@ -24,16 +24,16 @@ Configuration IISInstallConfiguration
         }
 	    Script BindingPort 
 		{
-            GetScript = { return @{ 'Result' = "Binding port ${Port}" } }
+            GetScript = { return @{ 'Result' = "Binding port 8080" } }
             TestScript = { return $false }
-            SetScript = { C:\Windows\System32\inetsrv\appcmd.exe set SITE "Default Web Site" /bindings:http/*:${Port}: }
+            SetScript = { C:\Windows\System32\inetsrv\appcmd.exe set SITE "Default Web Site" /bindings:http/*:8080: }
             DependsOn = "[WindowsFeature]ASP"
 		}
         Script OpeningPort
 		{
-            GetScript = { return @{ 'Result' = "Opening port ${Port}" } }
+            GetScript = { return @{ 'Result' = "Opening port 8080" } }
             TestScript = { return $false }
-            SetScript = { netsh advfirewall firewall add rule name="Zoo TCP Port ${Port}" dir=in action=allow protocol=TCP localport=${Port} }
+            SetScript = { netsh advfirewall firewall add rule name="Zoo TCP Port 8080" dir=in action=allow protocol=TCP localport=8080 }
             DependsOn = "[Script]BindingPort"
 		}
         
