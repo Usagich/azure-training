86c493cab5568be690dcdaf78c9a9708664f70b2
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-24 15:28:32 +0300

    upgrading run.ps

--- task4/run.ps1
+++ task4/run.ps1
@@ -9,7 +9,7 @@ param
 )
 
 Login-AzureRmAccount 
-Set-AzureRmContext -SubscriptionId $subscriptionId 
+Set-AzureRmContext -SubscriptionId $subscriptionId
 
 ##resource group creation
 New-AzureRmResourceGroup -Name $resourceGroupName -Location "West Europe"
@@ -21,7 +21,14 @@ $passwordVM = "qqq111QQQ111"
 New-AzureRmKeyVault -VaultName $vaultName -ResourceGroupName $resourceGroupName -Location "West Europe" -EnabledForTemplateDeployment
 Register-AzureRmResourceProvider -ProviderNamespace "Microsoft.KeyVault"
 $secretValue = ConvertTo-SecureString $passwordVM -AsPlainText -Force
-Set-AzureRKeyVaultSecret -VaultName $vaultName -Name "secret" -SecretValue $secretValue
+
+#sorry, but you should manually add your access permissions here: 
+#https://portal.azure.com/#resource/subscriptions/b1d40bc1-2977-4394-b374-fe62498046e2/resourceGroups/task4RG/providers/Microsoft.KeyVault/vaults/VMPasswordVault/access_policies
+#then you can create a secret
+Set-AzureKeyVaultSecret -VaultName $vaultName -Name "secret" -SecretValue $secretValue
+
+###$userEmail = (Get-AzureRmContext).Account.id
+###Set-AzureRmKeyVaultAccessPolicy -VaultName $vaultName -ResourceGroupName $resourceGroupName -UserPrincipalName $userEmail -PermissionsToKeys get,update -PermissionsToSecrets set,get -PermissionsToCertificates get -PassThru
 
 
 #####deploy to azure blob
@@ -31,7 +38,7 @@ Set-AzureRmContext -Subscription $subscriptionName
 #$storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourseGroupName -Name $storageAccountName).Value[0]
 $context = $storageAccount.Context
 ####upload file to blob container
-$filePath = "D:\azure_training\azure-training\task4\init.json", "D:\azure_training\azure-training\task4\vmdeploy.json", "D:\azure_training\azure-training\task4\vmdeploy.parameters.json"
+$filePath = ".\init.json", ".\vmdeploy.json", ".\vmdeploy.parameters.json"
 foreach ($path in $filePath)
 {
     $fileName = $path.Split('\')[-1]
