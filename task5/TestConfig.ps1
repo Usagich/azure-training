d882974212771cbdfb8cd7e1227a8c3a1a27bd39
Aliaksandra Navitskaya <Aliaksandra_Navitskaya@epam.com>
2018-01-26 19:19:23 +0300

    task5 DSC

---
+++ task5/TestConfig.ps1
@@ -0,0 +1,23 @@
+﻿Configuration TestConfig
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node "task5WebServer"
+    {
+        WindowsFeature IIS
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+            IncludeAllSubFeature = $true
+        }
+    }
+
+    Node "task5NWebServer"
+    {
+        WindowsFeature IIS
+        {
+            Ensure = "Absent"
+            Name = "Web-Server"
+        }
+    }
+}
